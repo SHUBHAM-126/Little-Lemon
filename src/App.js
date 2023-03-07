e2e21a58dd0a9669487ca700a29b7d99ecbec779
SHUBHAM-126 <shubhammhatre126@gmail.com>
2023-03-08 00:04:23 +0530

    Added tailwind and developed the Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
   );
 }
 
